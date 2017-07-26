3899ec2f5c098d6c25634079c05b8d80909e4223
Liu Jing <jing.liu@rea-group.com>
2017-07-26 17:39:25 +0800

    add background file

--- app/src/main/java/com/example/jingliu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jingliu/myapplication/MainActivity.java
@@ -33,5 +33,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+//        getSupportActionBar().setTitle("");
     }
 }
