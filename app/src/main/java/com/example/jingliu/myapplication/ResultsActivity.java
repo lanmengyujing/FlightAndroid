beb8a12a660d3eae65fe9bf3de1bf58cba6d6bea
Liu Jing <jing.liu@rea-group.com>
2017-07-27 17:32:00 +0800

    recycleView add manager and adapter

--- app/src/main/java/com/example/jingliu/myapplication/ResultsActivity.java
+++ app/src/main/java/com/example/jingliu/myapplication/ResultsActivity.java
@@ -2,6 +2,9 @@ package com.example.jingliu.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class ResultsActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class ResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        RecyclerView recycleView = (RecyclerView) findViewById(R.id.list_view);
+        recycleView.setLayoutManager(new LinearLayoutManager(this));
+        recycleView.setAdapter(new SearchResultAdapter());
     }
 }
---
+++ app/src/main/java/com/example/jingliu/myapplication/SearchResultAdapter.java
@@ -0,0 +1,22 @@
+package com.example.jingliu.myapplication;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+class SearchResultAdapter extends RecyclerView.Adapter {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
